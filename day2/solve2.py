048fc44d6b72602891a7218a19b5b2634d5d6d8b
Bruno Sienkiewicz <bruno.sienkiewicz@xtb.com>
2024-12-03 14:45:53 +0100

    solved day 3

--- day2/solve2.py
+++ day2/solve2.py
@@ -1,8 +1,5 @@
-def is_safe(line):
-    nums = [int(n) for n in line.split(" ")]
-
+def check_nums(nums):
     inc = 3 if nums[0] < nums[1] else -3
-
     if nums[0] < nums[1]:
         for i in range(1, len(nums)):
             if nums[i - 1] + inc < nums[i]:
@@ -21,8 +18,22 @@ def is_safe(line):
                 return False
     else:
         return False
-
     return True
+
+
+def is_safe(line):
+    nums = [int(n) for n in line.split(" ")]
+
+    if check_nums(nums):
+        return True
+
+    for i in range(0, len(nums)):
+        _nums = nums.copy()
+        _nums.pop(i)
+        if check_nums(_nums):
+            return True
+
+    return False
 
 
 with open("input.txt") as f:
