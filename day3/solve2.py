237b0f1e0a0948d0c8641f333624e8c38ff6a7c9
Bruno Sienkiewicz <bruno.sienkiewicz@xtb.com>
2024-12-03 12:48:44 +0100

    solve2 update

--- day3/solve2.py
+++ day3/solve2.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 
 with open("input1.txt") as f:
     input = f.read()
@@ -9,6 +10,8 @@ donts_iter = re.finditer(r"don\'t\(\)", input)
 
 do = next(dos_iter).start()
 dont = next(donts_iter).start()
+prev_do = 0
+prev_dont = -1
 
 res = 0
 
@@ -21,28 +24,29 @@ res = 0
 
 for m in re.finditer(r"mul\(\d+,\d+\)", input):
     mul = input[m.start() : m.end()]
-    print(mul, do, dont, m.start(), res)
 
     while dont < m.start():
+        prev_dont = dont
         try:
             dont = next(donts_iter).start()
         except StopIteration:
             dont = 999998
 
     while do < m.start():
+        prev_do = do
         try:
             do = next(donts_iter).start()
         except StopIteration:
             do = 999999
 
-    if do > dont:
-        n = mul.replace("mul(", "")
-        n = n.replace(")", "")
-        a, b = int(n.split(",")[0]), int(n.split(",")[1])
-        res += a * b
+    print(f"do={prev_do}", f"dont={prev_dont}", f"start={m.start()}", res)
+    if prev_dont < m.start() and prev_do < prev_dont:
+        print("dont mul")
         continue
-    if do < dont:
-        continue
+    n = mul.replace("mul(", "")
+    n = n.replace(")", "")
+    a, b = int(n.split(",")[0]), int(n.split(",")[1])
+    res += a * b
 
 
 print(res)
